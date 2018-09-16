2f10ff5b1b3d871a22571bd7daea3051062a910a
rhijmans <r.hijmans@gmail.com>
2018-09-16 14:06:52 -0700

    remove object files

--- src/rasterize.cpp
+++ src/rasterize.cpp
@@ -15,16 +15,16 @@ std::vector<double> rasterize_polygon(std::vector<double> r, double value, std::
 	std::vector<unsigned> nCol(n);
   
 	for (size_t row=0; row<nrows; row++) {
-  
-		double y = ymax - (row+0.5) * ry;
-		
-		//  Build a list of nodes.
+ 
+		double y = ymax - (row+0.5) * ry;		
+		//  Get nodes.
 		unsigned nodes = 0; 
 		size_t j = n-1;
 		for (size_t i=0; i<n; i++) {
 			if (((pY[i] < y) && (pY[j] >= y)) || ((pY[j] < y) && (pY[i] >= y))) {
-				double nds = (((pX[i] - xmin + (y-pY[i])/(pY[j]-pY[i]) * (pX[j]-pX[i])) + 0.5 * rx ) / rx); 
+				double nds = ((pX[i] - xmin + (y-pY[i])/(pY[j]-pY[i]) * (pX[j]-pX[i])) + 0.5 * rx ) / rx; 
 				nds = nds < 0 ? 0 : nds;
+		        nds = nds > ncols ? ncols : nds;
 				nCol[nodes] = (unsigned) nds;
 				nodes++;
 			}
@@ -32,14 +32,12 @@ std::vector<double> rasterize_polygon(std::vector<double> r, double value, std::
 		}
 		
 		std::sort(nCol.begin(), nCol.begin()+nodes);
+		unsigned ncell = ncols * row;
 		
-		//  Fill the pixels between node pairs.
+		//  Fill the cells between node pairs.
 		for (size_t i=0; i < nodes; i+=2) {
-			if (nCol[i] >= ncols) break;
-			if (nCol[i+1] > 0) {
-				if (nCol[i] < 0) nCol[i]=0 ;
-				if (nCol[i+1] >= ncols) nCol[i+1] = ncols;
-				int ncell = ncols * row;
+			if (nCol[i+1] > 0 && nCol[i] < ncols) {
+			//if (nCol[i] >= ncols || nCol[i+1] <= 0) break;
 				for (size_t col = nCol[i]; col < nCol[i+1]; col++) {
 					r[col + ncell] = value;
 				}
