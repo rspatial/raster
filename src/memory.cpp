5e6b7823a6241e3fbc4c03a7b39d79f652d6173e
rhijmans <r.hijmans@gmail.com>
2018-11-01 15:55:58 -0700

    mac mem

--- src/memory.cpp
+++ src/memory.cpp
@@ -5,7 +5,10 @@
 #include "sys/sysinfo.h"
 #include <unistd.h>
 #elif __APPLE__
-//#include mac hdr	
+#include <mach/vm_statistics.h>
+#include <mach/mach_types.h>
+#include <mach/mach_init.h>
+#include <mach/mach_host.h>
 #endif
 
 // [[Rcpp::export(name = ".availableRAM")]]
@@ -21,9 +24,25 @@ double availableRAM(double ram) {
 		sysinfo (&memInfo);
 		ram = memInfo.freeram;
 	#else
+		vm_size_t page_size;
+		mach_port_t mach_port;
+		mach_msg_type_number_t count;
+		vm_statistics64_data_t vm_stats;
+
+		mach_port = mach_host_self();
+		count = sizeof(vm_stats) / sizeof(natural_t);
+		if (KERN_SUCCESS == host_page_size(mach_port, &page_size) &&
+			KERN_SUCCESS == host_statistics64(mach_port, HOST_VM_INFO,
+											(host_info64_t)&vm_stats, &count)) {
+			long long free_memory = (int64_t)vm_stats.free_count * (int64_t)page_size;
+
+//			long long used_memory = ((int64_t)vm_stats.active_count +
+//									 (int64_t)vm_stats.inactive_count +
+//									 (int64_t)vm_stats.wire_count) *  (int64_t)page_size;
+			ram = free_memory;
+		}
 		// mac
-	    // perhaps use this
-		// https://stackoverflow.com/questions/38490320/how-to-query-amount-of-allocated-memory-on-linux-and-osx
+		//https://stackoverflow.com/questions/63166/how-to-determine-cpu-and-memory-consumption-from-inside-a-process
 	#endif
 	
 	return ram;
