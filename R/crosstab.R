6a1734ac08026932b41faba5a1efa7ebff017bc9
rhijmans <r.hijmans@gmail.com>
2018-11-16 10:22:03 -0800

    crosstab minor

--- R/crosstab.R
+++ R/crosstab.R
@@ -60,27 +60,18 @@ setMethod('crosstab', signature(x='RasterStackBrick', y='missing'),
 			res <- aggregate(res[, ncol(res), drop=FALSE], res[, 1:(ncol(res)-1), drop=FALSE], sum)
 		}
 		for (i in 1:(ncol(res)-1)) {
+		# get rid of factors
 			res[,i] <- as.numeric(as.character(res[,i]))
 		}
-		
 		if (nrow(res) == 0) {
 			res <- data.frame(matrix(nrow=0, ncol=length(nms)+1))
 		} 
-		colnames(res) <- c(nms, 'Freq')
-			
+		colnames(res) <- c(nms, 'Freq')	
 		if (! useNA ) {
 			i <- apply(res, 1, function(x) any(is.na(x)))
 			res <- res[!i,  ,drop=FALSE]
 		}
-		 
-			# keep NA classes if there are any
-		for (i in 1:(ncol(res)-1)) {
-			if (any(is.na(res[,i]))) {
-#				res[,i] <- factor(res[,i], levels=c(levels(res[,i]), NA), exclude=NULL) 
-			}
-			res[,i] <- as.numeric(res[,i])
-		}
-		
+	 		
 		if (!long) {
 			f <- eval(parse(text=paste('Freq ~ ', paste(nms , collapse='+'))))
 			res <- stats::xtabs(f, data=res, addNA=useNA)
