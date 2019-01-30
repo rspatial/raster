0a0cfcf0ee155b6abe537f2980bddc599bc501de
rhijmans <r,hijmans@gmail.com>
2019-01-30 10:30:15 -0800

    cleaner

--- R/rasterFromGDAL.R
+++ R/rasterFromGDAL.R
@@ -245,7 +245,7 @@
 	maxv[maxv == 4294967295] <- -Inf
 	try ( datatype <- .getRasterDType ( GDType[1] ), silent=TRUE )
 	
-	if ( all(is.finite(minv)) && all(is.finite(maxv)) ) {
+	if ( all(c(is.finite(minv), is.finite(maxv)))) {
 		r@data@haveminmax <- TRUE 
 	}
 	r@file@datanotation <- datatype
