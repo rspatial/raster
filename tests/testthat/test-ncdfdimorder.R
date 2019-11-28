ac8ed04bfb6fecf9b42f56f8dae206767999387e
Kassel Hingee <kassel.hingee@gmail.com>
2019-11-28 17:58:53 +1100

    added a test for 2D files

--- tests/testthat/test-ncdfdimorder.R
+++ tests/testthat/test-ncdfdimorder.R
@@ -35,3 +35,29 @@ test_that("netCDF file is correctly read with dimension order (3, 1, 2)", {
 # getValues(b, 1, 1)
 
 unlink(tmpfilename)
+
+
+############################### 2 dimensional file
+#create a temporary netCDF file
+r <- raster::raster(array(1:(10*15), dim= c(10, 15)), crs = CRS("+proj=longlat +datum=WGS84"))
+#the array dimension of length 10 becomes latitude, and is represented as rows in the data matrix of r
+#the array dimension of length 15 becomes longitude, and is represented as columns in the data matrix of r
+tmpfilename <- tempfile()
+tmpfilename <- paste0(tmpfilename, ".nc")
+writeRaster(r, tmpfilename)
+
+#preparing comparison values
+testpt1 <- SpatialPoints(matrix(c(0.0912,0.30001), ncol = 2, byrow = TRUE), proj4string = CRS(proj4string(r)))
+testpt1_vals <- extract(r, testpt1)
+testpt1r <- SpatialPoints(matrix(c(1 - 0.30001, 0.0912), ncol = 2, byrow = TRUE), proj4string = CRS(proj4string(r)))
+
+test_that("netCDF file is correctly read with dimension order (2, 1)", {
+  b <- raster::raster(tmpfilename, dims = c(2, 1))
+  expect_equal(dim(b), c(15, 10, 1))  #latitude (rows in r's matrix) become columns in b, longitude (cols in r's matrix) becomes rows in b
+  expect_equal(dim(as.matrix(b)), dim(as.matrix(r))[c(2, 1)])
+  expect_equal(length(getValues(b, 1)), 10)
+  vals <- extract(b, testpt1r)
+  expect_equivalent(vals, testpt1_vals)
+})
+
+unlink(tmpfilename)
