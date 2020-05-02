ce4f8a40a62ce519dcb13f3776a0cfa1545ca017
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-05-01 18:15:15 -0700

    crs

--- R/rasterToPoints.R
+++ R/rasterToPoints.R
@@ -17,7 +17,7 @@ rasterToPoints <- function(x, fun=NULL, spatial=FALSE, ...) {
 	if (! inherits(x, 'RasterStack' )) {
 		if ( ! fromDisk(x) & ! inMemory(x) ) {
 			if (spatial) {
-				crs <- x@proj4string
+				crs <- proj4string(x)
 				return(SpatialPoints(coords=xyFromCell(x, 1:ncell(x)), proj4string=crs) )
 			} else {
 				return(xyFromCell(x, 1:ncell(x)))
@@ -81,7 +81,7 @@ rasterToPoints <- function(x, fun=NULL, spatial=FALSE, ...) {
 	}
 	
 	if (spatial) {
-		crs <- x@proj4string
+		crs <- proj4string(x)
 		if (nrow(xyv) == 0) {
 			xyv <- rbind(xyv, 0)
 			v <- data.frame(xyv[ ,-c(1:2), drop=FALSE])
