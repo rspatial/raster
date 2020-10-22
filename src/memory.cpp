a511a046d9010f38d06a94b04a02cb83466a3cc1
Ben Fasoli <benfasoli@gmail.com>
2020-10-22 12:19:20 -0700

    Include inactive pages in available memory estimates on MacOS

--- src/memory.cpp
+++ src/memory.cpp
@@ -34,11 +34,8 @@ double availableRAM(double ram) {
 		if (KERN_SUCCESS == host_page_size(mach_port, &page_size) &&
 			KERN_SUCCESS == host_statistics64(mach_port, HOST_VM_INFO,
 											(host_info64_t)&vm_stats, &count)) {
-			long long free_memory = (int64_t)vm_stats.free_count * (int64_t)page_size;
-
-//			long long used_memory = ((int64_t)vm_stats.active_count +
-//									 (int64_t)vm_stats.inactive_count +
-//									 (int64_t)vm_stats.wire_count) *  (int64_t)page_size;
+			long long free_memory = ((int64_t)vm_stats.free_count +
+                               (int64_t)vm_stats.inactive_count) * (int64_t)page_size;
 			ram = free_memory;
 		//https://stackoverflow.com/questions/63166/how-to-determine-cpu-and-memory-consumption-from-inside-a-process
 		}
